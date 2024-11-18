59f3b5fb01ad0206be1e7aa9de1add7a1c36cd7f
Tb <intercablenoti@gmail.com>
2024-11-18 10:09:00 -0400

    Update Chats.java

--- Chats.java
+++ Chats.java
@@ -8,7 +8,7 @@ final static Scanner scanner = new Scanner(System.in); /*Se crea el objeto Scann
 se usa la nueva palabra clave seguida de una llamada al constructor de la clase Scanner. 
 new es la instancia de Scanner*/
 public static void main(String[] args){ /*Función principal en la aplicación en Java*/
- System.out.println("Bienvenido a la tienda virtual de Tecnot. Elige una de las opciones"); /*Le da la bienvenida al cliente cuando le da al input. */
+ System.out.println("Hola soy tú asistente virtual BEN de Tecnot. Elige una de estas opcione"); /*Le da la bienvenida al cliente cuando le da al input. */
  ConversacionOpciones(); /*Llama a la función de Conversación y Opciones. */
 }
 static void ConversacionOpciones (){
@@ -16,18 +16,16 @@ while (true) {  /*Bucle que ejecutará indefinidamente las convesaciones y opcio
 String converResp = scanner.nextLine(); /*El método nextLine() de java.util.Scanner es la clase de escáner de la línea actual y 
 devuelve la entrada que se omitió. En este caso las conversaciones y respuestas.*/
 
-if (converResp.equals("Telefonos disponibles")) { /*El método equals() compara dos cadenas y devuelve verdadero si las cadenas son iguales.*/
-System.out.println("Claro, aquí tienes una lista: Galaxy S2 y Redmin 2Q");
+if (converResp.equals("Falla de internet")) { /*El método equals() compara dos cadenas y devuelve verdadero si las cadenas son iguales.*/
+System.out.println("Si tiene problemas de conexión de internet por favor comunicarse al servicio técnico: 0902139.");
        
-} else if (converResp.equals("Ofertas de Telefonos")) { /*else if es la condición basada en ese valor preexistente. 
+} else if (converResp.equals("Costo de instalación")) { /*else if es la condición basada en ese valor preexistente. 
 Si esa condición es verdadera. En este caso las conversaciones y opciones ya previamente programadas.*/
-System.out.println("Actualmente, tenemos las siguientes ofertas:Redmin 2Q ¡10% de descuento!.");
+System.out.println("Claro el costo de la instación es de 30$ y de banda acha 120$ y fibra optica 200$.");
        
-}else if (converResp.equals("Comparacion de modelos")) {
-System.out.println("Claro aquí los detalles: Galaxy S2 4GB RAM, y Redmin 2Q 3GB RAM.");
-} else if (converResp.equals("Precio de Telefonos")) {
-System.out.println("Claro el preio de Galaxy S2 esta disponible en 280$ y Redmin 2Q 180$.");
-      
+}else if (converResp.equals("Ubicacion")) {
+System.out.println("Los ubucamos en la calle Félix en la esquina de la plaza. Te puedes comunicar al teléfono: 902139.");
+ 
 } else if (converResp.equals("Mas informacion")) {
 System.out.println("Puedes comunicarte con nuestro equipo. al Correo electrónico: soporte@tecnot.com.");
 }else if (converResp.equals("Finalizar chat")) {
